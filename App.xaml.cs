2acc5729aafce38fb1567318b15f8796841609ac
Christian-Labuanan <christianmichaellabuanan09@gmail.com>
2024-02-19 20:57:51 +0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace STI_ONN
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
