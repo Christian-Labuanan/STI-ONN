f191ddd3f990f8e4305c15cb38885ec94a63ecae
Christian-Labuanan <christianmichaellabuanan09@gmail.com>
2024-03-14 18:49:27 +0800

    added new window home

--- Home.xaml.cs
+++ Home.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,27 +10,25 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace STI_ONN
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Home.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Home : Window
     {
-        public MainWindow()
+        public Home()
         {
             InitializeComponent();
-            this.SizeToContent = SizeToContent.WidthAndHeight;
         }
 
         private void touch_Click_1(object sender, RoutedEventArgs e)
         {
             this.Hide();
-            Home h1 = new Home();
-            h1.Show();
+            MainWindow m1 = new MainWindow();   
+            m1.Show();
         }
     }
 }
