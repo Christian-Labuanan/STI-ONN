77eb76293b6bea90b08fb2d7c296c71ff7b096c3
Christian-Labuanan <christianmichaellabuanan09@gmail.com>
2024-03-24 21:57:35 +0800

    added a scroll view
    updated the functions that can zoom in and out the image for the map that act as a template

--- Building.xaml.cs
+++ Building.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace STI_ONN
 {
@@ -19,6 +20,7 @@ namespace STI_ONN
     /// </summary>
     public partial class Building : Window
     {
+        private double _zoomLevel = 1.0;
         public Building()
         {
             InitializeComponent();
@@ -29,6 +31,42 @@ namespace STI_ONN
             this.Hide();
             Home home = new Home(); 
             home.Show();
+        }
+
+        private void ZoomIn_Click(object sender, RoutedEventArgs e)
+        {
+            Zoom(1.1);
+        }
+
+        private void ZoomOut_Click(object sender, RoutedEventArgs e)
+        {
+            Zoom(0.9);
+        }
+
+        private void Zoom(double zoomFactor)
+        {
+            _zoomLevel *= zoomFactor;
+            var transform = new ScaleTransform(_zoomLevel, _zoomLevel);
+            image.LayoutTransform = transform;
+            CenterImage();
+        }
+
+        private void CenterImage()
+        {
+            double offsetX = (canvas.ActualWidth - (image.ActualWidth * _zoomLevel)) / 2;
+            double offsetY = (canvas.ActualHeight - (image.ActualHeight * _zoomLevel)) / 2;
+            Canvas.SetLeft(image, offsetX);
+            Canvas.SetTop(image, offsetY);
+        }
+
+        private void Canvas_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            double zoomFactor = e.Delta > 0 ? 1.1 : 0.9;
+            Zoom(zoomFactor);
+        }
+        private void Canvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            CenterImage();
         }
     }
 }
