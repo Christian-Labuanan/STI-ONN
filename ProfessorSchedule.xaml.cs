44589ef930945b382f04cc94f9b19f331fdcdf01
Roddoma <152410201+Roddoma@users.noreply.github.com>
2024-10-12 12:34:35 +0800

    ProfSched
    adding profsched button in home thats all

---
+++ ProfessorSchedule.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace STI_ONN
+{
+    /// <summary>
+    /// Interaction logic for ProfessorSchedule.xaml
+    /// </summary>
+    public partial class ProfessorSchedule : Window
+    {
+        public ProfessorSchedule()
+        {
+            InitializeComponent();
+        }
+        private void touch_Click_1(object sender, RoutedEventArgs e)
+        {
+            Home main = new Home();
+            main.Show();
+            this.Close();
+        }
+    }
+}
